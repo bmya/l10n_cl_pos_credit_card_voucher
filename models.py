239e38088116ce2d157a9bf0ee66946c463eaff3
ufaks <excellm@yandex.ru>
2015-09-18 10:54:21 +0500

    done

---
+++ models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+class invoice_seller_field(models.Model):
+    _inherit = "account.invoice"
+
+    cc_voucher = fields.Char('Credit Card Voucher'),
+
+
+class account_journal(models.Model):
+    _inherit = "account.journal"
+
+    add_credit_card_voucher_number = fields.Boolean("Add credit card voucher number", default=False)
